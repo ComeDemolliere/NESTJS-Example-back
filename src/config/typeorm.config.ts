1e7bba2b48463e21eee7f86dcd2f8564c30127e3
ComeDemolliere <come.demolliere@etu.unice.fr>
2019-07-09 10:25:05 +0200

    add typeorm config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { UseGuards } from '@nestjs/common';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'user',
+    password: 'userpwd',
+    database: 'TOMdatabase',
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: true,
+};
